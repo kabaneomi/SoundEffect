f659504b334058bdff08c37ba9725ede903e47b4
kabaneomi <kabaneomi@gmail.com>
2012-11-27 00:17:05 +0900

    add common func

---
+++ SoundApplication/cCommonFunc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoundApplication
+{
+    class cCommonFunc
+    {
+        public void converIntValue(ref int bit, byte[] info, int size)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                bit = BitConverter.ToInt32(info.Reverse().ToArray(), size);
+            }
+            else
+            {
+                bit = BitConverter.ToInt32(info, size);
+            }
+        }
+    }
+}
